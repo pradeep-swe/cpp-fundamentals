52eac1987ba91fad44242ef92a04910ffa6639de
Pradeep Pandey <dev.pradeeep@gmail.com>
2025-10-28 16:30:25 +0530

    Text formatted

--- measure-ball-height.cpp
+++ measure-ball-height.cpp
@@ -1,5 +1,5 @@
 /*
-    Program to simulate a ball being dropped off of a tower. To start, the user is asked for the height of the tower in meters. Assuming normal gravity (9.8 m/s2), and that the ball has no initial velocity (the ball is stationary at start). Have the program output the height of the ball above the ground after 0, 1, 2, 3, 4, and 5 seconds. The ball should not go underneath the ground (height 0).
+    Program to simulate a ball being dropped off of a tower. To start, the user is asked for the height of the tower in meters. <br> Assuming normal gravity (9.8 m/s2), and that the ball has no initial velocity (the ball is stationary at start). Have the program <br> output the height of the ball above the ground after 0, 1, 2, 3, 4, and 5 seconds. The ball should not go underneath the ground (height 0). <br>
 */
 #include <iostream>
 
