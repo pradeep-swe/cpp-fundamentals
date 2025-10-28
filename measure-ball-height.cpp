b3415aa1d5491891bd7a18e5721c78ccd1acf902
Pradeep Pandey <dev.pradeeep@gmail.com>
2025-10-28 16:36:12 +0530

    File Formatted

--- measure-ball-height.cpp
+++ measure-ball-height.cpp
@@ -6,6 +6,7 @@ the ground (height 0).
 */
 #include <iostream>
 
+
 // Get the Tower height form the user.
 double getHeight() {
     std::cout << "Enter the towerHeight of the tower in meters: ";
@@ -15,10 +16,12 @@ double getHeight() {
     return towerHeight;
 }
 
+
 // Display the distance the ball travelled over the time.
 void displayDistance(double towerHeight, double time) {
     std::cout << "The towerHeight of the tower at " << time << "sec is: " << towerHeight << " meters." << std::endl;
 }
+
 
 void calculateDistance(double towerHeight, double initialVelocity, double gravity) {
     /*
@@ -39,6 +42,7 @@ void calculateDistance(double towerHeight, double initialVelocity, double gravit
             return;
     }
 }
+
 
 int main() {
     double gravity{9.8};
