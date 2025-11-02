999e8d8f0616fbaa5a893d1511df6d2e5d613971
Pradeep Pandey <dev.pradeeep@gmail.com>
2025-11-02 18:39:40 +0530

    file formatted

--- is_even.cpp
+++ is_even.cpp
@@ -1,9 +1,11 @@
 /*
-Write a program that asks the user to input an integer, and tells the user whether the number is even or odd. Write a constexpr function called isEven() that returns true if an integer passed to it is even, and false otherwise. Use the remainder operator to test whether the integer parameter is even. Make sure isEven() works with both positive and negative numbers.
+Write a program that asks the user to input an integer, and tells the user whether the number is even or odd. Write a
+constexpr function called isEven() that returns true if an integer passed to it is even, and false otherwise. Use the 
+remainder operator to test whether the integer parameter is even. Make sure isEven() works with both positive and 
+negative numbers.
 */
 
 #include <iostream>
-#include <uint32_t>
 
 
 // Ask user for a number
