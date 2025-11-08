91c0a293a601ce9168c1c81405db5417361513ef
Pradeep Pandey <pradeep.community@gmail.com>
2025-11-08 10:44:00 +0530

    files modified

--- basic-arithmetic-calculator.cpp
+++ basic-arithmetic-calculator.cpp
@@ -2,10 +2,10 @@
 #include "constants.h"
 
 /*
-Write a function called calculate() that takes two integers and a char representing one of the following mathematical operations: +, -, *, /, or % 
-(remainder). Use a switch statement to perform the appropriate mathematical operation on the integers, and return the result. If an invalid 
-operator is passed into the function, the function should print an error message. For the division operator, do an integer division, and don’t 
-worry about divide by zero.
+Write a function called calculate() that takes two integers and a char representing one of the following mathematical 
+operations: +, -, *, /, or % (remainder). Use a switch statement to perform the appropriate mathematical operation 
+on the integers, and return the result. If an invalid operator is passed into the function, the function should print
+an error message. For the division operator, do an integer division, and don’t worry about divide by zero.
 */
 
 
